02d1b11c3913b8a791354ca00e9dde6a4047a31f
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2023-06-12 20:28:42 -0300

    chore: add @ComponentScan in WishlistApplication

--- src/main/java/com/elielbatiston/wishlist/WishlistApplication.java
+++ src/main/java/com/elielbatiston/wishlist/WishlistApplication.java
@@ -2,8 +2,10 @@ package com.elielbatiston.wishlist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.elielbatiston.wishlist")
 public class WishlistApplication {
 
 	public static void main(String[] args) {
