bf2f6f8021aecbfc1617673cbfaf88a1ca75f7a5
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2023-06-12 20:24:04 -0300

    feat: add getInternationalizedMessage and setLocale in InternationalizationConfig

--- src/main/java/com/elielbatiston/wishlist/config/InternationalizationConfig.java
+++ src/main/java/com/elielbatiston/wishlist/config/InternationalizationConfig.java
@@ -4,16 +4,28 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import java.util.Locale;
 
 @Configuration
 public class InternationalizationConfig extends ReloadableResourceBundleMessageSource {
 
+    private Locale locale;
+
+    private final ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+
     @Bean
     public MessageSource messageSource() {
-        ReloadableResourceBundleMessageSource messageSource =
-                new ReloadableResourceBundleMessageSource();
         messageSource.setBasename("classpath:messages/messages");
         messageSource.setUseCodeAsDefaultMessage(true);
         return messageSource;
+    }
+
+    public String getInternationalizedMessage(final String key, final Object... parameters) {
+        return messageSource().getMessage(key, parameters, this.locale);
+    }
+
+    public void setLocale(final Locale locale) {
+        this.locale = locale;
+        messageSource.setDefaultLocale(this.locale);
     }
 }
