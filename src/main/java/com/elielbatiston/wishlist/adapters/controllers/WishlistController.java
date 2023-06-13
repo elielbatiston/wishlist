6739641f0122612ebc53f959ba04f0bfada5e0bb
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2023-06-12 23:22:23 -0300

    feat: add getAProduct method

--- src/main/java/com/elielbatiston/wishlist/adapters/controllers/WishlistController.java
+++ src/main/java/com/elielbatiston/wishlist/adapters/controllers/WishlistController.java
@@ -1,12 +1,10 @@
 package com.elielbatiston.wishlist.adapters.controllers;
 
 import com.elielbatiston.wishlist.usecases.AddProductToWishlistUseCase;
+import com.elielbatiston.wishlist.usecases.FindAProductInWishlistUseCase;
 import com.elielbatiston.wishlist.usecases.FindAllCustomerProductsUseCase;
 import com.elielbatiston.wishlist.usecases.RemoveProductFromWishlistUseCase;
-import com.elielbatiston.wishlist.usecases.dto.InputFindAllCustomerProductsDTO;
-import com.elielbatiston.wishlist.usecases.dto.InputAddProductToWishlistDTO;
-import com.elielbatiston.wishlist.usecases.dto.InputRemoveProductFromWishlist;
-import com.elielbatiston.wishlist.usecases.dto.OutputFindAllCustomerProductsDTO;
+import com.elielbatiston.wishlist.usecases.dto.*;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,7 +17,8 @@ public class WishlistController {
 
     public static final String WISHLIST_RESOURCE = "/wishlist";
     private static final String WISHLIST_DELETE_A_PRODUCT_ENDPOINT = "/{idCustomer}/product/{idProduct}";
-    public static final String WISHLIST_GET_WISHLIST_ENDPOINT = "/{idCustomer}";
+    private static final String WISHLIST_GET_WISHLIST_ENDPOINT = "/{idCustomer}";
+    private static final String WISHLIST_GET_A_PRODUCT_IN_WISHLIST_ENDPOINT = "/{idCustomer}/{idProduct}";
 
     @Autowired
     private AddProductToWishlistUseCase addProductToWishlistUseCase;
@@ -29,6 +28,9 @@ public class WishlistController {
 
     @Autowired
     private FindAllCustomerProductsUseCase findAllCustomerProductsUseCase;
+
+    @Autowired
+    private FindAProductInWishlistUseCase findAProductInWishlistUseCase;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -54,6 +56,17 @@ public class WishlistController {
     public ResponseEntity<OutputFindAllCustomerProductsDTO> getWishlist(@PathVariable final String idCustomer) {
         final InputFindAllCustomerProductsDTO input = new InputFindAllCustomerProductsDTO(idCustomer);
         final OutputFindAllCustomerProductsDTO output = findAllCustomerProductsUseCase.execute(input);
+        return ResponseEntity.ok().body(output);
+    }
+
+    @GetMapping(WISHLIST_GET_A_PRODUCT_IN_WISHLIST_ENDPOINT)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<OutputFindAProductDTO> getAProduct(
+        @PathVariable final String idCustomer,
+        @PathVariable final String idProduct
+    ) {
+        final InputFindAProductDTO input = new InputFindAProductDTO(idCustomer, idProduct);
+        final OutputFindAProductDTO output = findAProductInWishlistUseCase.execute(input);
         return ResponseEntity.ok().body(output);
     }
 }
